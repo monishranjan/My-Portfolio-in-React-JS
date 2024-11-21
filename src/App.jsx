48b72fcf02c3724db409b78d4f6f002c79a9bbca
Monish <monishranjan9@gmail.com>
2024-11-21 23:03:45 +0530

    Added Under development component and some minor error and bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
 import LeafCursor from './components/Cursor/LeafCursor'
+import UnderDevelopment from './components/UnderDevelopmentSticker'
 
 function App() {
   return (
     <>
       {/* <LeafCursor/> */}
+      <UnderDevelopment />
       <Home/>
     </>
   )
---
+++ src/components/UnderDevelopmentSticker.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const UnderDevelopment = () => {
+  return (
+    <div className="fixed left-4 top-28 z-50">
+      <div className="bg-yellow-500 text-black px-4 py-2 rounded-md shadow-lg flex items-center gap-2 animate-pulse">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span className="font-semibold">This site is under development!</span>
+      </div>
+    </div>
+  );
+};
+
+export default UnderDevelopment;
